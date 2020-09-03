8aa4b299f72ba83b3d2628958a523a1190bd9f7d
Garrett Fitzgerald <garrettfitzgerald@MacBook-Pro.lan>
2020-09-02 20:27:33 -0500

    fixed fault migration file

--- migrations/sqls/20200830194030-seed-up.sql
+++ migrations/sqls/20200830194030-seed-up.sql
@@ -12,12 +12,9 @@ CREATE SEQUENCE IF NOT EXISTS games_id_seq;
 
 CREATE TABLE "games" (
     "id" int4 NOT NULL DEFAULT nextval('games_id_seq'::regclass),
-    "userId" int4,
-    "runningScore" int4,
     "createdAt" timestamp,
     "updatedAt" timestamp,
     "completed" bool NOT NULL DEFAULT false,
-    CONSTRAINT "games_user_id_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id"),
     PRIMARY KEY ("id")
 );
 
@@ -25,15 +22,27 @@ CREATE SEQUENCE IF NOT EXISTS frames_id_seq;
 
 CREATE TABLE "frames" (
     "id" int4 NOT NULL DEFAULT nextval('frames_id_seq'::regclass),
-    "userId" int4 NOT NULL,
-    "gameId" int4 NOT NULL,
-    "attempts" int4 DEFAULT 0,
-    "score" int4 DEFAULT 0,
-    "frameNumber" int4 NOT NULL,
+    "game_id" int4 NOT NULL,
+    "frame_number" int4 NOT NULL,
     "createdAt" timestamp,
     "updatedAt" timestamp,
-    "allowedAttempts" int4 DEFAULT 2,
-    CONSTRAINT "frames_user_id_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id"),
-    CONSTRAINT "frames_game_id_fkey" FOREIGN KEY ("gameId") REFERENCES "games"("id"),
+    PRIMARY KEY ("id")
+);
+
+CREATE SEQUENCE IF NOT EXISTS scores_id_seq;
+
+
+CREATE TABLE "scores" (
+    "id" int4 NOT NULL DEFAULT nextval('scores_id_seq'::regclass),
+    "frame_id" int4 NOT NULL,
+    "score" int4,
+    "user_id" int4 NOT NULL,
+    "game_id" int4 NOT NULL,
+    "attempt" int4,
+    "createdAt" timestamp,
+    "updatedAt" timestamp,
+    CONSTRAINT "scores_frame_id_fkey" FOREIGN KEY ("frame_id") REFERENCES "frames"("id"),
+    CONSTRAINT "scores_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id"),
+    CONSTRAINT "scores_game_id_fkey" FOREIGN KEY ("game_id") REFERENCES "games"("id"),
     PRIMARY KEY ("id")
 );
